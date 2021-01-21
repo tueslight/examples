4ad83fca37bcb7ca267041e73877be2e848a5bd8
mq <maqiang3@longshine.com>
2021-01-21 15:11:00 +0800

    hello springboot

---
+++ springboot-demo/src/test/java/com/tueslight/demo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tueslight.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
